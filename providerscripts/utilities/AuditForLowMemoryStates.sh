dbd42929c08a50f17d987bf32ef92155aa4175ee
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-02-26 20:36:09 +0000

    Update AuditForLowMemoryStates.sh

--- providerscripts/utilities/AuditForLowMemoryStates.sh
+++ providerscripts/utilities/AuditForLowMemoryStates.sh
@@ -30,7 +30,7 @@ then
     /bin/mkdir -p ${HOME}/config/lowmemoryaudit/database/${IP}
 fi
 
-if ( [ "${MEMORY}" -lt "100000" ] )
+if ( [ "${MEMORY}" -lt "$1" ] )
 then
     /bin/echo "LOW MEMORY state detected `/bin/date` VALUE: ${MEMORY} KB remaining" >> ${HOME}/config/lowmemoryaudit/${IP}/lowmemoryaudittrail.dat
     ${HOME}/providerscripts/email/SendEmail.sh "LOW MEMORY STATE DETECTED" "LOW MEMORY state detected `/bin/date` VALUE: ${MEMORY} KB remaining on machine with ip address: `${HOME}/providerscripts/utilities/GetPublicIP.sh`"
