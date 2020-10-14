efc28d26f5dd797938be83058843a068d0fc41b6
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-10-14 11:04:45 +0100

    Update InstallMariaDB.sh

--- applicationdb/maria/InstallMariaDB.sh
+++ applicationdb/maria/InstallMariaDB.sh
@@ -51,6 +51,8 @@ then
         fi
         /bin/sed -i '/^\[mysqld\]/a character-set-server = utf8mb4' /etc/mysql/my.cnf
         /bin/sed -i '/^\[mysqld\]/a collation-server = utf8mb4_bin' /etc/mysql/my.cnf
+    elif ( [ -f ${HOME}/.ssh/DATABASEDBaaSINSTALLATIONTYPE:Maria ] )
+        /bin/sed -i '1s/^/SET SESSION sql_require_primary_key = 0;\n/' ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
     fi
 
     #Not sure why but sometimes installation of the application is truncated leaving only a partial set of tables installed
