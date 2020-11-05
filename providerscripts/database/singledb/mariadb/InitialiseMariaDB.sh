40e3a1c174cf6b53de32c499d3f297c38cd92adb
Peter C Winter <agile-deployer@users.noreply.github.com>
2020-11-05 19:22:40 +0000

    Update InitialiseMariaDB.sh

--- providerscripts/database/singledb/mariadb/InitialiseMariaDB.sh
+++ providerscripts/database/singledb/mariadb/InitialiseMariaDB.sh
@@ -52,11 +52,10 @@ GRANT ALL PRIVILEGES ON ${DB_N}.* TO \"${DB_U}\"@\"${ipmask}\" IDENTIFIED BY \"$
 flush privileges;" > ${HOME}/runtime/initialiseDB.sql
 else
     /bin/echo "use mysql;
-CREATE USER \"${DB_U}\";
+CREATE USER \"${DB_U}\" IDENTIFIED BY \"${DB_P}\";
 flush privileges;
 create database ${DB_N};
-GRANT ALL PRIVILEGES ON mysql.* TO \"${DB_U}\"@'localhost' IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON mysql.* TO \"${DB_U}\"@'127.0.0.1' IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO \"${DB_U}\"@'localhost';
 GRANT ALL PRIVILEGES ON ${DB_N}.* TO \"${DB_U}\"@'localhost' IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON ${DB_N}.* TO \"${DB_U}\"@'127.0.0.1' IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON ${DB_N}.* TO \"${DB_U}\"@\"${HOST}\" IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;
