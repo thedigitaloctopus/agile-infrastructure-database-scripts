c1d125e13609fe7f9b0b042a0e5b2b86132c8691
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-15 10:25:43 +0100

    Update InstallPostgresFromSource.sh

--- installscripts/postgres/InstallPostgresFromSource.sh
+++ installscripts/postgres/InstallPostgresFromSource.sh
@@ -15,8 +15,36 @@ cd postgresql-${version}
 ./configure --without-readline
 /usr/bin/make
 /usr/bin/make install
+/usr/sbin/adduser --disabled-password --gecos \"\" postgres ; /bin/echo postgres:${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/chpasswd ;
 /usr/bin/mkdir /usr/local/pgsql/data
 /usr/bin/chown postgres:postgres /usr/local/pgsql/data
 /usr/bin/su - postgres
 /usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data/
 /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data/ -l logfile start
+
+/bin/echo "#!/bin/bash
+
+/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data/ -l logfile start
+
+exit 0" > /etc/rc.local
+
+/bin/chmod +x /etc/rc.local
+
+/bin/echo "[Unit]
+Description=/etc/rc.local Compatibility
+Documentation=man:systemd-rc-local-generator(8)
+ConditionFileIsExecutable=/etc/rc.local
+After=network.target
+ 
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+RemainAfterExit=yes
+GuessMainPID=no
+ 
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/rc-local.service
+
+/usr/bin/systemctl enable rc-local.service
+/usr/bin/systemctl start rc-local.service
