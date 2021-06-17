194053cac85538191ddac13ae6d592a328d7c0ab
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-17 17:07:47 +0100

    Update InitialiseDatabaseConfig.sh

--- providerscripts/database/singledb/postgres/InitialiseDatabaseConfig.sh
+++ providerscripts/database/singledb/postgres/InitialiseDatabaseConfig.sh
@@ -1,3 +1,28 @@
+#!/bin/sh
+########################################################################################
+# Description: This script will setup the postgres database ready for the application
+# to be installed. It will be setup with the postgres user having its password set to
+# $DB_P which is available in ${HOME}/credentials/shit
+#
+# Author: Peter Winter
+# Date: 17/06/2021
+########################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
 if ( [ -f ${HOME}/runtime/POSTGRES_CONFIGURED ] )
 then
     exit
