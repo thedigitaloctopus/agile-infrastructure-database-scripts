4ab8560757be60ebff77bc520bfc04e8e33faf42
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-17 17:05:25 +0100

    Update InitialisePostgresDB.sh

--- providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
+++ providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
@@ -18,12 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ####################################################################################
 ####################################################################################
-set -x
-
-OUT_FILE="postie-build-out-`/bin/date | /bin/sed 's/ //g'`"
-exec 1>>${HOME}/logs/${OUT_FILE}
-ERR_FILE="postie-build-err-`/bin/date | /bin/sed 's/ //g'`"
-exec 2>>${HOME}/logs/${ERR_FILE}
+#set -x
 
 ipmask="`/bin/ls ${HOME}/.ssh/IPMASK:* | /usr/bin/awk -F':' '{print $NF}'`"
 DB_PORT="`/bin/ls ${HOME}/.ssh/DB_PORT:* | /usr/bin/awk -F':' '{print $NF}'`"
@@ -50,7 +45,6 @@ then
     fi
 
     /bin/rm ${postgres_pid}
-  #  /bin/sed -i 's/md5/trust/g' ${postgres_config}
     /bin/sed -i "/listen_addresses/c\        listen_addresses = '*'" ${postgres_sql_config}
     /bin/sed -i "/^port/c\        port = ${DB_PORT}" ${postgres_sql_config}
     /bin/sed -i "/^#port/c\        port = ${DB_PORT}" ${postgres_sql_config}
@@ -68,45 +62,8 @@ then
         /bin/echo "host       ${DB_N}              ${DB_U}            ${ipmask}/16          md5" >> ${postgres_config}
         /bin/echo "host       all              postgres            127.0.0.1/32         trust" >> ${postgres_config}
     fi
-
-   # /usr/sbin/service postgresql restart
     
     . ${HOME}/providerscripts/database/singledb/postgres/InitialiseDatabaseConfig.sh
-   # if ( [ "$?" != "0" ] )
-   # then
-   #     count="0"
-   #     while ( [ "${count}" -lt "5" ] )
-   #     do
-   #         /usr/bin/su postgres -c "/usr/local/pgsql/bin/pg_ctl restart -D /usr/local/pgsql/data/ -l /home/postgres/logfile"
-   #         if ( [ "$?" = "0" ] )
-   #         then
-   #             count="5"
-   #         else
-   #            count="`/usr/bin/expr ${count} + 1`"
-   #            /bin/sleep 10
-   #         fi
-   #     done
-   # fi
-    
-   # /usr/bin/sudo -u postgres /usr/bin/psql -h 127.0.0.1 -p ${DB_PORT} template1 -c "CREATE USER ${DB_U} WITH ENCRYPTED PASSWORD '${DB_P}';"
-   # /usr/bin/sudo -u postgres /usr/bin/psql -h 127.0.0.1 -p ${DB_PORT} template1 -c "ALTER USER ${DB_U} WITH SUPERUSER;"
-   # /usr/bin/sudo -u postgres /usr/bin/psql -h 127.0.0.1 -p ${DB_PORT} template1 -c "CREATE DATABASE ${DB_N} WITH OWNER ${DB_U} ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' TEMPLATE template0;"
-   # if ( [ "$?" != "0" ] )
-   # then   
-   #     /usr/bin/sudo -u postgres /usr/bin/psql -h 127.0.0.1 -p ${DB_PORT} template1 -c "CREATE DATABASE ${DB_N} WITH OWNER ${DB_U} ENCODING 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8' TEMPLATE template0;"
-   # fi
-   # /usr/bin/sudo -u postgres /usr/bin/psql -h 127.0.0.1 -p ${DB_PORT} template1 -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_N} to ${DB_U};"
-   # /usr/bin/sudo -u postgres /usr/bin/psql -h 127.0.0.1 -p ${DB_PORT} template1 -c "ALTER USER postgres PASSWORD '${DB_P}';"
-  # 
-   # /bin/sed -i "s/trust/md5/g" ${postgres_config}
-
-  #  /bin/rm ${postgres_pid}
-            
-  # /usr/sbin/service postgresql reload
-  # if ( [ "$?" != "0" ] )
-  # then
-  #    /usr/bin/su postgres -c "/usr/local/pgsql/bin/pg_ctl restart -D /usr/local/pgsql/data/ -l /home/postgres/logfile"   
-  # fi
 
 elif ( [ -f ${HOME}/.ssh/DATABASEDBaaSINSTALLATIONTYPE:Postgres ] )
 then
