930157b0f2c37d75b0bc256efa3ef9bce419cabf
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-24 22:16:54 +0100

    Update GetIP.sh

--- providerscripts/utilities/GetIP.sh
+++ providerscripts/utilities/GetIP.sh
@@ -24,10 +24,9 @@
 BUILDOS="`/bin/ls ${HOME}/.ssh/BUILDOS:* | /usr/bin/awk -F':' '{print $NF}'`"
 BUILDOSVERSION="`/bin/ls ${HOME}/.ssh/BUILDOSVERSION:* | /usr/bin/awk -F':' '{print $NF}'`"
 
-if ( [ -f ${HOME}/EXOSCALE ] && [ ! -f ${HOME}/runtime/PRIVATEIPAVAILABLE ] )
+if ( [ -f ${HOME}/EXOSCALE ] )
 then
     /usr/sbin/dhclient
-    /bin/touch ${HOME}/runtime/PRIVATEIPAVAILABLE
 fi
 
 if ( [ -f ${HOME}/VULTR ] && [ ! -f ${HOME}/runtime/NETCONFIGURED ] )
