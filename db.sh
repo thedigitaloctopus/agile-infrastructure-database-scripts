2ee6079ea412c99e4f39db7f8081de3ac37c6846
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-25 16:40:10 +0100

    Update db.sh

--- db.sh
+++ db.sh
@@ -141,6 +141,16 @@ fi
 /bin/echo "vm.panic_on_oom=1
 kernel.panic=10" >> /etc/sysctl.conf
 
+#Double down on preventing logins as root. We already tried, but, make absolutely sure because we can't guarantee format of /etc/ssh/sshd_config
+
+if ( [ "`/bin/grep '^#PermitRootLogin' /etc/ssh/sshd_config`" != "" ] || [ "`/bin/grep '^PermitRootLogin' /etc/ssh/sshd_config`" != "" ] )
+then
+    /bin/sed -i "s/^PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
+    /bin/sed -i "s/^#PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
+else
+    /bin/echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+fi
+
 /bin/echo "${0} `/bin/date`: Installing software ...." >> ${HOME}/logs/MonitoringLog.dat
 #Update and upgrade the software to its latest available versions
 
