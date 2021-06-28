8f9f4b98740cb4a8aaee2031fe47691bcd0fac1f
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-28 12:06:57 +0100

    Update InstallMariaDBServer.sh

--- installscripts/InstallMariaDBServer.sh
+++ installscripts/InstallMariaDBServer.sh
@@ -27,11 +27,11 @@ fi
 
 if ( [ "${BUILDOS}" = "" ] )
 then
-    BUILDOS="`/bin/ls ${HOME}/.ssh/BUILDOS:* | /usr/bin/awk -F':' '{print $NF}'`"
+    BUILDOS="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOS'`"
 fi
+    
+BUILDOSVERSION="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOSVERSION'`"
 
-BUILDOSVERSION="`/bin/ls ${HOME}/.ssh/BUILDOSVERSION:* | /usr/bin/awk -F':' '{print $NF}'`"
-#version="`curl https://downloads.mariadb.org/ | grep stable | /bin/sed 's/.*>MariaDB //g' | /usr/bin/awk '{print $1}' | /usr/bin/head -1 2>/dev/null`"
 version="`/usr/bin/curl https://downloads.mariadb.org/ | /bin/grep stable | /usr/bin/head -1 | /bin/sed 's/[^0-9.]*//g' | /bin/sed 's/\./ /g' | /usr/bin/xargs | /bin/sed 's/ /./g'`"
 
 DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
