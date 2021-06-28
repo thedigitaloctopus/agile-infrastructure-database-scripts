b61744bdc953f3e2b5283ddd9c422490fa5aa716
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-28 16:16:47 +0100

    Update CustomiseMySQLByApplication.sh

--- applicationdb/mysql/CustomiseMySQLByApplication.sh
+++ applicationdb/mysql/CustomiseMySQLByApplication.sh
@@ -22,7 +22,7 @@
 
 HOST=""
 
-if (  [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS-secured`" = "1" ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS-secured`" = "1" ] )
 then
     HOST="127.0.0.1"
 elif ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
@@ -32,7 +32,7 @@ else
     HOST="localhost"
 fi
 
-if (  [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh APPLICATION:moodle`" = "1" ] )
 then
     /bin/sed -i '/^\[mysqld\]/a binlog_format=mixed' /etc/mysql/my.cnf
     /bin/sed -i '/^\[mysqld\]/a innodb_large_prefix=1' /etc/mysql/my.cnf
