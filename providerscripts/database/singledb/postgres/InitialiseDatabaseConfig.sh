55fd11999662994f12aa71c9aa4cd1bd627d2f64
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-28 12:54:46 +0100

    Update InitialiseDatabaseConfig.sh

--- providerscripts/database/singledb/postgres/InitialiseDatabaseConfig.sh
+++ providerscripts/database/singledb/postgres/InitialiseDatabaseConfig.sh
@@ -28,7 +28,7 @@ then
     exit
 fi
 
-DB_PORT="`/bin/ls ${HOME}/.ssh/DB_PORT:* | /usr/bin/awk -F':' '{print $NF}'`"
+DB_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DB_PORT'`"
 
 DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
 DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
