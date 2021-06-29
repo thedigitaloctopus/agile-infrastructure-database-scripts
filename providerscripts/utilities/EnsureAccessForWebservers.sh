ac2206f4e9a06abc5c2b7130cce39dad063c11c2
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-06-29 10:51:57 +0100

    Update EnsureAccessForWebservers.sh

--- providerscripts/utilities/EnsureAccessForWebservers.sh
+++ providerscripts/utilities/EnsureAccessForWebservers.sh
@@ -34,12 +34,12 @@ if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLAT
 then
     for webserverip in `/bin/ls ${HOME}/config/webserverips`
     do
-        ipmask="`/bin/echo ${webserverip} | /usr/bin/cut -d "." -f -2`.%.%"
+        IP_MASK="`/bin/echo ${webserverip} | /usr/bin/cut -d "." -f -2`.%.%"
       #  if ( [ "`/bin/ls  ${HOME}/.ssh/CLOUDHOST:* | /usr/bin/awk -F':' '{print $NF}'`" = "exoscale" ] )
       #  then
       #      ipmask="10.0.%.%"
       #  fi 
-        /usr/bin/mysql -u ${DB_U} -p${DB_P} -e "GRANT ALL PRIVILEGES ON ${DB_N}.* TO \"${DB_U}\"@\"${ipmask}\" IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;"
+        /usr/bin/mysql -u ${DB_U} -p${DB_P} -e "GRANT ALL PRIVILEGES ON ${DB_N}.* TO \"${DB_U}\"@\"${IP_MASK}\" IDENTIFIED BY \"${DB_P}\" WITH GRANT OPTION;"
     done
 fi
 
@@ -48,19 +48,19 @@ then
     postgres_config="`/usr/bin/find / -name pg_hba.conf -print`"
     for webserverip in `/bin/ls ${HOME}/config/webserverips`
     do
-        ipmask="`/bin/echo ${webserverip} | /usr/bin/cut -d "." -f -2`.%.%"
+        IP_MASK="`/bin/echo ${webserverip} | /usr/bin/cut -d "." -f -2`.%.%"
         
       #  if ( [ "`/bin/ls  ${HOME}/.ssh/CLOUDHOST:* | /usr/bin/awk -F':' '{print $NF}'`" = "exoscale" ] )
       #  then
       #      ipmask="10.0.%.%"
       #  fi 
         
-        ipmask="`/bin/echo ${ipmask} | /bin/sed 's/%/0/g'`"
+        IP_MASK="`/bin/echo ${IP_MASK} | /bin/sed 's/%/0/g'`"
 
-        if ( [ "`/bin/cat ${postgres_config} | /bin/grep ${DB_N} | /bin/grep ${ipmask}`" = "" ] )
+        if ( [ "`/bin/cat ${postgres_config} | /bin/grep ${DB_N} | /bin/grep ${IP_MASK}`" = "" ] )
         then
-            ipmask="`/bin/echo ${webserverip} | /usr/bin/cut -d "." -f -2`"
-            /bin/echo "host       ${DB_N}              ${DB_U}            ${ipmask}.0.0/0          md5" >> ${postgres_config}
+            IP_MASK="`/bin/echo ${webserverip} | /usr/bin/cut -d "." -f -2`"
+            /bin/echo "host       ${DB_N}              ${DB_U}            ${IP_MASK}.0.0/0          md5" >> ${postgres_config}
             /usr/sbin/service postgresql reload
             if ( [ "$?" != "0" ] )
             then
