1b14bc1e5fce647ca410ea17731a59d77e51f2a1
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-03 18:51:07 +0100

    Update AuditForLowMemoryStates.sh

--- providerscripts/utilities/AuditForLowMemoryStates.sh
+++ providerscripts/utilities/AuditForLowMemoryStates.sh
@@ -22,7 +22,7 @@
 #set -x
 
 
-MEMORY="`/bin/cat /proc/meminfo | grep MemFree | /usr/bin/awk '{print $2}'`"
+MEMORY="`/bin/grep MemFree /proc/meminfo | /usr/bin/awk '{print $2}'`"
 IP="`${HOME}/providerscripts/utilities/GetIP.sh`"
 
 if ( [ ! -d ${HOME}/config/lowmemoryaudit/database/${IP} ] )
