b7064755f58a252ab4bd041bdfc4218d2eaabadc
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-05 20:54:54 +0100

    Update InstallPostgresClient.sh

--- installscripts/InstallPostgresClient.sh
+++ installscripts/InstallPostgresClient.sh
@@ -31,8 +31,8 @@ then
 
     /usr/bin/wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     /bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-    /usr/bin/apt update
-    /usr/bin/apt -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
+    /usr/bin/apt-get update
+    /usr/bin/apt-get -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
     #/usr/bin/apt-get -qq -y install postgresql-client
 fi
 
@@ -42,8 +42,8 @@ then
 
     /usr/bin/wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     /bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-    /usr/bin/apt update
-    /usr/bin/apt -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
+    /usr/bin/apt-get update
+    /usr/bin/apt-get -y install postgresql-client-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
     #/usr/bin/apt-get -qq -y install postgresql-client
 fi
 
