6618559a337f024ed336ad0af43a942fe5307d00
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-07-09 22:11:01 +0100

    Create ResetPassword.sh

---
+++ providerscripts/database/singledb/mysql/ResetPassword.sh
@@ -0,0 +1,17 @@
+HOST=""
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
+then
+    HOST="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DBaaSHOSTNAME'`"
+else
+    HOST="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYPUBLICIP'`"
+fi
+
+IP_MASK="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'IPMASK'`"
+DB_PORT="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'DBPORT'`"
+
+/bin/echo "use mysql;
+ALTER USER \"${username}\"@'localhost' IDENTIFIED BY "${password}";
+ALTER USER \"${username}\"@'127.0.0.1' IDENTIFIED BY "${password}";
+ALTER USER \"${username}\"@\"${HOST}\" IDENTIFIED BY "${password}";
+ALTER USER \"${username}\"@\"${IP_MASK}\" IDENTIFIED BY "${password}";
+" > ${HOME}/runtime/resetpasswordDB.sql
