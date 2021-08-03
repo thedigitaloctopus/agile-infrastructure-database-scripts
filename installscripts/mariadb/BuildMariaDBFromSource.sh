fd21855e864a41d1daaced139f37494afb705674
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-03 21:06:08 +0100

    Update BuildMariaDBFromSource.sh

--- installscripts/mariadb/BuildMariaDBFromSource.sh
+++ installscripts/mariadb/BuildMariaDBFromSource.sh
@@ -12,10 +12,9 @@
 cd server
 mariadb_version="`/usr/bin/wget -qO- https://mariadb.com/kb/en/release-notes/ | /bin/grep "MariaDB" | /bin/grep "Release Notes" | /usr/bin/head -n 1 | /bin/sed 's/.*MariaDB //g' | /usr/bin/awk -F' ' '{print $1}'`"
 /usr/bin/git checkout ${mariadb_version}
-#./BUILD/compile-amd64-max
+./BUILD/compile-amd64-max
 
 #/usr/bin/cmake . -DBUILD_CONFIG=mysql_release && make -j8
-/usr/bin/cmake -DBUILD_CONFIG=mysql_release
 /usr/bin/make
 /usr/bin/make install
 /usr/sbin/mysqld
