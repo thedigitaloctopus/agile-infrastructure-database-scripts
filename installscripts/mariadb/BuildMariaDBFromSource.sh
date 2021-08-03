1af4d2d283abb966aee5c4b480b7212e0bb285cf
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-03 20:23:03 +0100

    Update BuildMariaDBFromSource.sh

--- installscripts/mariadb/BuildMariaDBFromSource.sh
+++ installscripts/mariadb/BuildMariaDBFromSource.sh
@@ -1,6 +1,10 @@
 # /etc/apt/sources.list
 # uncomment deb-src
 # run apt update
+#deb http://ftp.debian.org/debian buster main contrib
+#deb http://security.debian.org buster/updates main contrib
+#deb-src http://ftp.debian.org/debian buster main contrib
+#deb-src  http://security.debian.org buster/updates main contrib
 
 /usr/bin/apt-get install -qq -y build-essential bison cmake
 /usr/bin/apt-get -qq -y build-dep mariadb-server 
@@ -8,6 +12,10 @@
 cd server
 mariadb_version="`/usr/bin/wget -qO- https://mariadb.com/kb/en/release-notes/ | /bin/grep "MariaDB" | /bin/grep "Release Notes" | /usr/bin/head -n 1 | /bin/sed 's/.*MariaDB //g' | /usr/bin/awk -F' ' '{print $1}'`"
 /usr/bin/git checkout ${mariadb_version}
-/usr/bin/cmake . -DBUILD_CONFIG=mysql_release && make -j8
-make install
+#./BUILD/compile-amd64-max
+
+#/usr/bin/cmake . -DBUILD_CONFIG=mysql_release && make -j8
+/usr/bin/cmake -DBUILD_CONFIG=mysql_release
+/usr/bin/make
+/usr/bin/make install
 /usr/sbin/mysqld
