834118948fa27f3b136c41fa18146b6574099e82
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-08-30 15:37:18 +0100

    Update GetIP.sh

--- providerscripts/utilities/GetIP.sh
+++ providerscripts/utilities/GetIP.sh
@@ -37,7 +37,7 @@ then
 
     if ( [ "${BUILDOS}" = "debian" ] )
     then
-        if ( [ "${BUILDOSVERSION}" = "9" ] || [ "${BUILDOSVERSION}" = "10" ] )
+        if ( [ "${BUILDOSVERSION}" = "9" ] || [ "${BUILDOSVERSION}" = "10" ] || [ "${BUILDOSVERSION}" = "11" ] )
         then
             /bin/echo "auto ens7
 iface ens7 inet static
