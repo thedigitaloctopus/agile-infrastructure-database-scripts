be9b2a124466ad5d485d6ef0a56f7f401f1701c7
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-08 16:11:15 +0100

    Update GetIP.sh

--- providerscripts/utilities/GetIP.sh
+++ providerscripts/utilities/GetIP.sh
@@ -20,6 +20,11 @@
 #######################################################################################################
 #set -x
 
+if ( [ -f ${HOME}/EXOSCALE ] )
+then
+    /usr/sbin/dhclient 1>/dev/null 2>/dev/null
+fi
+
 IP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYIP'`"
 if ( [ "`/usr/bin/ip addr | /bin/grep ${IP}`" != "" ] )
 then
@@ -30,14 +35,6 @@ fi
 
 BUILDOS="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOS'`"
 BUILDOSVERSION="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOSVERSION'`"
-
-
-if ( [ -f ${HOME}/EXOSCALE ] )
-then
-    /usr/sbin/dhclient
-fi
-
-IP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYIP'`"
 
 if ( [ -f ${HOME}/VULTR ] && [ ! -f ${HOME}/runtime/NETCONFIGURED ] )
 then
