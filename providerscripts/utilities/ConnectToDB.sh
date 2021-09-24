ea4bd9657f661bc3e7c308d38dc3783b73831110
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-09-24 15:27:40 +0100

    Update ConnectToDB.sh

--- providerscripts/utilities/ConnectToDB.sh
+++ providerscripts/utilities/ConnectToDB.sh
@@ -9,5 +9,5 @@ if ( [ "${1}" = "" ] )
 then
     /usr/bin/mysql -u ${DB_U} -p${DB_P} ${DB_N} --host="${HOST}" --port="${DB_PORT}"
 else 
-    /usr/bin/mysql -u ${DB_U} -p${DB_P} ${DB_N} --host="${HOST}" --port="${DB_PORT}" -e "${1}"
+    /usr/bin/mysql -s -u ${DB_U} -p${DB_P} ${DB_N} --host="${HOST}" --port="${DB_PORT}" -e "${1}"
 fi
