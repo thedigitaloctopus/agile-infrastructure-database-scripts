f6b6c5858c18049b2e37c6eb88d7f0297ff90024
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-10-31 20:55:53 +0000

    Create InstallGITLFS.sh

---
+++ installscripts/InstallGITLFS.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version="`/usr/bin/curl https://github.com/git-lfs/git-lfs/ | /bin/grep -ow "v[0-9].*\.[0-9].*\.[0-9].*" | /bin/sed 's/<.*//g' | /usr/bin/tail -1`"
+/usr/bin/wget https://github.com/git-lfs/git-lfs/releases/download/${version}/git-lfs-linux-amd64-${version}.tar.gz
+/bin/mkdir git-lfs
+/bin/tar xvfz git-lfs-linux-amd64-${version}.tar.gz -C ./git-lfs
+/bin/sh ./git-lfs/install.sh
+/bin/rm -r git-lfs
