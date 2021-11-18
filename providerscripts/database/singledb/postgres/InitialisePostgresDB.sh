1576ea37dbb9d81b655772bef1078c9c5ebec216
Agile Deployer <agile-deployer@users.noreply.github.com>
2021-11-18 20:49:26 +0000

    Update InitialisePostgresDB.sh

--- providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
+++ providerscripts/database/singledb/postgres/InitialisePostgresDB.sh
@@ -68,6 +68,10 @@ then
     export PGPASSWORD="${DB_P}" && /usr/bin/psql -h ${HOST} -U ${DB_U} -p ${DB_PORT} -d template1 -c "CREATE DATABASE ${DB_N} ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';"
     if ( [ "$?" != "0" ] )
     then
+        export PGPASSWORD="${DB_P}" && /usr/bin/psql -h ${HOST} -U ${DB_U} -p ${DB_PORT} -d defaultdb -c "CREATE DATABASE ${DB_N} ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';"
+    fi
+    if ( [ "$?" != "0" ] )
+    then
         export PGPASSWORD="${DB_P}" && /usr/bin/psql -h ${HOST} -U ${DB_U} -p ${DB_PORT} -d template1 -c "CREATE DATABASE ${DB_N} ENCODING 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';"
     fi
 fi
