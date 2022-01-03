d21b69ead1002a8a6726c06c5d2c6b0b94ddfc85
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-03 21:30:55 +0000

    Update Update.sh

--- installscripts/Update.sh
+++ installscripts/Update.sh
@@ -28,7 +28,7 @@ fi
 if ( [ "${BUILD_OS}" = "ubuntu" ] )
 then
     /usr/bin/yes | /usr/bin/dpkg --configure -a
-    /usr/bin/apt install -y -qq apt-utils 2&1>/dev/null
+    /usr/bin/apt-get install -y -qq apt-utils 2&1>/dev/null
     /usr/bin/apt-get -qq -y update --allow-change-held-packages
     while ( [ "$?" != "0" ] )
     do 
@@ -40,7 +40,7 @@ fi
 if ( [ "${BUILD_OS}" = "debian" ] )
 then
     /usr/bin/yes | /usr/bin/dpkg --configure -a
-    /usr/bin/apt install -y -qq apt-utils 2&1>/dev/null
+    /usr/bin/apt-get install -y -qq apt-utils 2&1>/dev/null
     /usr/bin/apt-get -qq -y update --allow-change-held-packages
     while ( [ "$?" != "0" ] )
     do 
