785c11ff8bdb5defe581cbcc12047d553aa6ecaf
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-01-03 21:29:37 +0000

    Update InstallMariaDBClient.sh

--- installscripts/InstallMariaDBClient.sh
+++ installscripts/InstallMariaDBClient.sh
@@ -46,7 +46,7 @@ then
         do
             version="`/bin/echo ${version} | /bin/awk -F'.' '{print $1"."$2}'`"
             /usr/bin/add-apt-repository "deb [arch=amd64] https://mirrors.ukfast.co.uk/sites/mariadb/repo/${version}/ubuntu focal main"
-            /usr/bin/apt -qq -y update
+            /usr/bin/apt-get -qq -y update
             if ( [ "$?" != "0" ] )
             then
                 /bin/sed -i '/ukfast/d'  /etc/apt/sources.list
@@ -78,7 +78,7 @@ then
         do
             version="`/bin/echo ${version} | /bin/awk -F'.' '{print $1"."$2}'`"
             /usr/bin/add-apt-repository "deb [arch=amd64,arm64,ppc64el] https://mirrors.ukfast.co.uk/sites/mariadb/repo/${version}/debian buster main"
-            /usr/bin/apt -qq -y update
+            /usr/bin/apt-get -qq -y update
             if ( [ "$?" != "0" ] )
             then
                /bin/sed -i '/ukfast/d'  /etc/apt/sources.list
@@ -98,7 +98,7 @@ then
         do
             version="`/bin/echo ${version} | /bin/awk -F'.' '{print $1"."$2}'`"
             /usr/bin/add-apt-repository "deb [arch=amd64,arm64,ppc64el] https://mirrors.ukfast.co.uk/sites/mariadb/repo/${version}/debian bullseye main"
-            /usr/bin/apt -qq -y update
+            /usr/bin/apt-get -qq -y update
             if ( [ "$?" != "0" ] )
             then
                /bin/sed -i '/ukfast/d'  /etc/apt/sources.list
