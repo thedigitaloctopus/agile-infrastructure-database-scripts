0a7a6e22e3dbdf39230dd6095aa0db361567e3e9
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-13 19:40:04 +0000

    Update InstallPostgres.sh

--- installscripts/InstallPostgres.sh
+++ installscripts/InstallPostgres.sh
@@ -33,18 +33,10 @@ then
         ${HOME}/installscripts/postgres/InstallPostgresFromSource.sh
     elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'POSTGRES:repo'`" = "1" ] )
     then
-        #/usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y update
-        #/usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install locales-all        
-        #/usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install postgresql postgresql-contrib
-        #version="`/bin/ls /etc/postgresql/`"
-        #/usr/bin/sudo -su postgres /usr/lib/postgresql/${version}/bin/postgres -D /var/lib/postgresql/${version}/main -c config_file=/etc/postgresql/${version}/main/postgresql.conf
-        #/usr/sbin/service postgresql restart
         
         version="`/usr/bin/curl https://www.postgresql.org/ftp/source/ | /bin/grep -o ">v.*<\/a" | /bin/sed 's/^>//g' | /bin/sed 's/<.*//g' | /bin/grep -v "rc" | /bin/grep -v "alpha" | /bin/grep -v "beta" | /usr/bin/head -1 | /bin/sed 's/v//g'`"
         /usr/bin/wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
         /bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-       # /usr/bin/apt-get update
-       # ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
         ${HOME}/installscripts/Update.sh ${BUILDOS}
         /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -y install postgresql-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
         version="`/bin/ls /etc/postgresql/`"
@@ -60,19 +52,10 @@ then
         /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install locales-all        
         ${HOME}/installscripts/postgres/InstallPostgresFromSource.sh    
     elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'POSTGRES:repo'`" = "1" ] )
-    then
-        #/usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y update
-        #/usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install locales-all        
-        #/usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install postgresql postgresql-contrib
-        #version="`/bin/ls /etc/postgresql/`"
-        #/usr/bin/sudo -su postgres /usr/lib/postgresql/${version}/bin/postgres -D /var/lib/postgresql/${version}/main -c config_file=/etc/postgresql/${version}/main/postgresql.conf
-        #/usr/sbin/service postgresql restart
-        
+    then       
         version="`/usr/bin/curl https://www.postgresql.org/ftp/source/ | /bin/grep -o ">v.*<\/a" | /bin/sed 's/^>//g' | /bin/sed 's/<.*//g' | /bin/grep -v "rc" | /bin/grep -v "alpha" | /bin/grep -v "beta" | /usr/bin/head -1 | /bin/sed 's/v//g'`"
         /usr/bin/wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
         /bin/echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-       # /usr/bin/apt-get update
-       # ${HOME}/installscripts/UpdateAndUpgrade.sh ${BUILDOS}
         ${HOME}/installscripts/Update.sh ${BUILDOS}
         /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -y install postgresql-`/bin/echo ${version} | /usr/bin/awk -F'.' '{print $1}'`
         version="`/bin/ls /etc/postgresql/`"
