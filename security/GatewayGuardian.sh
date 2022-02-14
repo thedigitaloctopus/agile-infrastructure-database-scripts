2aee5e1d2e099ed49053af5161b5fbb9ab0b4d1d
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-14 14:10:09 +0000

    Update GatewayGuardian.sh

--- security/GatewayGuardian.sh
+++ security/GatewayGuardian.sh
@@ -147,7 +147,6 @@ then
            /bin/echo "${username}:${user_password_digest}" >> ${HOME}/runtime/credentials/htpasswd
            /bin/sed -i "/${username}:/s/LIVE:   //g" ${HOME}/runtime/credentials/htpasswd_plaintext_history
            /bin/echo "LIVE:   ${username}:${user_password}:${email}" >> ${HOME}/runtime/credentials/htpasswd_plaintext_history
-           /bin/touch ${HOME}/config/credentials/GATEWAY_GUARDIAN_UPDATED
            ${HOME}/providerscripts/email/SendEmail.sh "YOUR NEW GATEWAY GUARDIAN PASSWORD" "YOUR NEW GATEWAY GUARDIAN PASSWORD IS (  ${user_password}  ).  Please enter it when prompted with your application username for access to ${WEBSITE_URL}" "${email}"
        fi
    done
