36ea1f478b4cfc4778544d6f3af0286ab02fc6a7
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-18 05:06:38 +0000

    Create StoreDatabaseCredentials.sh

---
+++ providerscripts/utilities/StoreDatabaseCredentials.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+/bin/echo "$1" > ${HOME}/config/credentials/shit
+/bin/echo "$2" >> ${HOME}/config/credentials/shit
+/bin/echo "$3" >> ${HOME}/config/credentials/shit
+
+/bin/echo "$1" > ${HOME}/credentials/shit
+/bin/echo "$2" >> ${HOME}/credentials/shit
+/bin/echo "$3" >> ${HOME}/credentials/shit
