c0c10e15da434bb0484545ee2e834b1963de63f0
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-19 20:04:26 +0000

    Update InstallMySQLDB.sh

--- applicationdb/mysql/InstallMySQLDB.sh
+++ applicationdb/mysql/InstallMySQLDB.sh
@@ -51,8 +51,6 @@ then
         /bin/sed -i '/SESSION.SQL_LOG_BIN/d' ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
         if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh DATABASEINSTALLATIONTYPE:DBaaS`" = "1" ] )
         then
-           # /bin/sed -i '/sql_require_primary_key/d' ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
-          #  /bin/sed -i 's/.*sql_require_primary_key.*/SET sql_require_primary_key=0;/g' ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
             if ( [ "`/bin/grep GTID ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql`" != "" ] )
             then
                 /bin/sed -i '/GTID_PURGED/d' ${HOME}/backups/installDB/${WEBSITE_NAME}DB.sql
