eb7f132b1b7fa455de82c74cad83fd4e3007b6e2
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-22 19:18:50 +0000

    Update Super.sh

--- providerscripts/utilities/Super.sh
+++ providerscripts/utilities/Super.sh
@@ -20,7 +20,7 @@
 ########################################################################################
 #set -x
 
-/bin/grep SERVERUSERPASSWORD database_configuration_settings.dat | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/sudo -S /bin/echo "Going Super hold on to your hat" 
+/bin/grep -a SERVERUSERPASSWORD database_configuration_settings.dat | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/sudo -S /bin/echo "Going Super hold on to your hat" 
 
 /bin/echo
 /bin/echo
