f30872e1cf63518a5871c1911759d57d4c2d4e4a
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-25 19:32:29 +0000

    Update PrimeCredentials.sh

--- providerscripts/utilities/PrimeCredentials.sh
+++ providerscripts/utilities/PrimeCredentials.sh
@@ -51,13 +51,13 @@ ${HOME}/providerscripts/utilities/UpdateIP.sh
 /bin/chmod 400 ${HOME}/credentials/shit
 /bin/chmod 400 ${HOME}/.ssh/shit
 
-DB_N="`/bin/sed '1q;d' ${HOME}/config/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/config/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/config/credentials/shit`"
+DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
-DB1_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
-DB1_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
-DB1_U="`/bin/sed '3q;d' ${HOME}/credentials/shit`"
+DB1_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+DB1_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+DB1_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
 if ( [ "${DB_N}" != "${DB1_N}" ] || [ "${DB_P}" != "${DB1_P}" ] ||  [ "${DB_U}" != "${DB1_U}" ] )
 then
