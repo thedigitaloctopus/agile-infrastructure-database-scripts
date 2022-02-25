9c00374a263dcffcb4acc38c51c36a90b28b2212
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-25 19:21:55 +0000

    Update InstallSingleDB.sh

--- providerscripts/database/singledb/InstallSingleDB.sh
+++ providerscripts/database/singledb/InstallSingleDB.sh
@@ -42,9 +42,9 @@ fi
 
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/CheckConfigDatastore.sh "credentials/shit"`" = "1" ]  )
 then
-    DB_N="`/bin/sed '1q;d' ${HOME}/config/credentials/shit`"
-    DB_P="`/bin/sed '2q;d' ${HOME}/config/credentials/shit`"
-    DB_U="`/bin/sed '3q;d' ${HOME}/config/credentials/shit`"
+    DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+    DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+    DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 else
     DB_N="`/bin/sed '1q;d' ${HOME}/credentials/shit`"
     DB_P="`/bin/sed '2q;d' ${HOME}/credentials/shit`"
