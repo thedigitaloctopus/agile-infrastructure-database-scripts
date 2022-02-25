619e84b1f54bdd65544cf3db491397cbb3a326f8
Agile Deployer <agile-deployer@users.noreply.github.com>
2022-02-25 19:24:05 +0000

    Update Backup.sh

--- providerscripts/git/Backup.sh
+++ providerscripts/git/Backup.sh
@@ -94,9 +94,9 @@ date="`/bin/date | /bin/sed 's/ //g'`"
 /bin/echo "${0} `/bin/date`: Backing up database" >> ${HOME}/logs/MonitoringLog.dat
 websiteDB="${HOME}/backups/${WEBSITE_NAME}-DB-backup".tar.gz
 
-DB_N="`/bin/sed '1q;d' ${HOME}/config/credentials/shit`"
-DB_P="`/bin/sed '2q;d' ${HOME}/config/credentials/shit`"
-DB_U="`/bin/sed '3q;d' ${HOME}/config/credentials/shit`"
+DB_N="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 1`"
+DB_P="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 2`"
+DB_U="`${HOME}/providerscripts/datastore/configwrapper/GetDBCredential.sh "credentials/shit" 3`"
 
 cd ${HOME}/backups
 
